9800b42fab3edca0b6897de341c6c8fa16748b44
Evan Schneider <evanrs@gmail.com>
2015-09-16 16:07:16 -0400

    Mock auth cycle

--- src/server.js
+++ src/server.js
@@ -4,59 +4,56 @@ const cookieParser = require('cookie-parser');
 const csrf = require('csurf');
 const passport = require('passport');
 
-const server = require('express')();
-const debug = require('debug')('cmyk:server');
 
-const Strategy = require('./strategy');
+const debug = require('debug')('cmyk:server');
+const server = require('express')();
+
+const {SECRET, withAuthenticatedUser} = require('./auth');
 
 // Redirect for HTTPS
+server.set('trust proxy', true);
 server.use(function (req, res, next) {
+  if (process.env.NODE_ENV !== 'production')
     return next();
-    // If https terminated by heroku, or express asserts the conn. is encrypted,
-    // or the protocol matches https, then we're good!
-    if (req.headers['x-forwarded-proto'] === SERVER.protocol ||
-        req.connection.encrypted ||
-        req.protocol === SERVER.protocol) {
-        res.header(
-            'Strict-Transport-Security', 'max-age=31536000; includeSubDomains');
 
-        next();
-    }
-    else res.redirect(`${SERVER.protocol}://${req.get('host')}${req.url}`);
+  // If https terminated by heroku, or express asserts the conn. is encrypted,
+  // or the protocol matches https, then we're good!
+  if (req.secure ||
+      req.headers['x-forwarded-proto'] === 'https' ||
+      req.connection.encrypted ||
+      req.protocol === 'https') {
+    res.header(
+      'Strict-Transport-Security', 'max-age=31536000; includeSubDomains');
+
+    next();
+  }
+  else
+    res.redirect(`https://${req.get('host')}${req.url}`);
 });
 
 server.use(compression());
 server.use(bodyParser.json());
-server.use(cookieParser());
+server.use(cookieParser(SECRET, {
+  secure: false,
+  domain: '.cmyk.nyc'
+}));
 server.use(csrf({cookie: true}));
 server.use(passport.initialize());
 
-passport.use(new Strategy({secret: 'secret'}, function (request, user, done) {
-  request.user = user;
-  done();
-}));
 
-function withAuthenticatedUser (request, response, next) {
-  passport.authenticate('cmyk', (err, user, info) =>
-    ! user ?
-      request.login(user, (err) =>
-        err ?
-          response.send(401)
-        : next())
-    : response.send(401)
-  )
-}
+server.use('/sanity', (req, res) => res.send({message: 'you\'re OK'}));
 
-server.use('/', withAuthenticatedUser, function (req, res, next) {
-  res.send({
-    message: 'hello'
-  });
-});
 
 server.use('/login', function (req, res, next) {
-  var token = require('jsonwebtoken').sign({id: 123, role: 'admin'}, 'secret');
-  res.cookie('authorization', token)
-  res.send(200);
+  var token = require('jsonwebtoken').sign({id: 123, role: 'admin'}, SECRET);
+  res.cookie('authorization', token);
+  res.sendStatus(200);
 });
 
-server.listen(3000);
+server.use('/', withAuthenticatedUser, function (req, res, next) {
+  res.send(req.user);
+  next();
+});
+
+var connection = server.listen(3000, () =>
+  debug(`Listening at port ${connection.address().port}`));
